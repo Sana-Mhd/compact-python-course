a3a065fd2048b6accfed466f7bd4b68c8c79e2d5
Sana-Mhd <sana2001mohammadi@gmail.com>
2026-09-22 19:27:11 +0330

    Create task3.py

---
+++ week1/task3.py
@@ -0,0 +1,20 @@
+a = 7
+a_float = float(a)
+print(a, "->", a_float, type(a_float))
+
+b = 9.8
+b_int = int(b)
+print(b, "->", b_int, type(b_int))
+
+c = 42
+c_str = str(c)
+print(c, "->", c_str, type(c_str))
+
+d = "123"
+d_int = int(d)
+print(d, "->", d_int, type(d_int))
+
+e = 0
+f = 5
+print(e, "->", bool(e))
+print(f, "->", bool(f))
